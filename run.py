f0967e333e7082f7954f1129f07242af9ba58c43
Ryu Jae Young <sharkgom@codingsiro.work>
2021-09-17 20:46:37 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 import discord, asyncio, os, json
 from discord.ext import commands
 from datetime import datetime
- 
+try:
+ f = json.loads(open("setup.json", "r").read())
+ token = f['token']
+expect:
+ print("setup.json 파일을 불러오는데 오류가 발생했어요 파일이 존재하거나 읽기 권한을 확인해주세요")
+ exit()
 bot = commands.Bot(command_prefix="%", intents=discord.Intents.all())
 
 for cogs in os.listdir("cogs"):
@@ -20,5 +25,13 @@ async def on_ready():
         await asyncio.sleep(5)
 
 
-
-bot.run(token)
+if token == "insert token here!":
+ print("""초기 설정이 되어있지 않은 모양이에요 
+ https://discord.com/developers/applications 이곳을 방문한 뒤
+ setup.json 파일을 열어 수정해주세요
+ """)
+ exit()
+elif toke == "":
+ print("토큰 값이 비어있는 것 같아요 제대로된 토큰을 넣어주세요")
+else:
+ bot.run(f['token'])
