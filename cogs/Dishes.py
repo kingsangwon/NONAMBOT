7bf9ed11c47427b5886146c2d4e146db3ba0e1b3
RyuJaeYoung <admin@codingsiro.work>
2021-09-25 20:50:18 +0900

    update Dishes.py

--- cogs/Dishes.py
+++ cogs/Dishes.py
@@ -1,18 +1,18 @@
-import discord
+import discord, datetime
 from discord.ext import commands
-
 from module.dish import dishFromDate
 
+tajiri = datetime.datetime.now().strftime('%Y%m%d')
 class Dish(commands.Cog):
     def __init__(self, bot):
         self.bot=bot
 
     @commands.command(name='급식')
-    async def dish(self, ctx, ymd):
-        breakfast=dishFromDate(ymd, 1)
-        lunch=dishFromDate(ymd, 2)
-        dinner=dishFromDate(ymd, 3)
-        embed=discord.Embed(title="급식", description=f'{ymd} 군산제일고등학교 급식입니다.', color=0x29428b)
+    async def dish(self, ctx):
+        breakfast=dishFromDate(tajiri, 1)
+        lunch=dishFromDate(tajiri, 2)
+        dinner=dishFromDate(tajiri, 3)
+        embed=discord.Embed(title="급식", description=f'{tajiri} 군산제일고등학교 급식입니다.', color=0x29428b)
         embed.add_field(
             name='조식',
             value='\n'.join(breakfast)
