54e64c5f3609b007f61427fce37908c22764350f
Ryu Jae Young <sharkgom@codingsiro.work>
2021-11-03 15:13:47 +0900

    Update Ping.py

--- cogs/Ping.py
+++ cogs/Ping.py
@@ -7,7 +7,7 @@ class Ping(commands.Cog):
         
     @commands.command(name='ping')
     async def ping(self, ctx):
-        await ctx.send('pong!')
+        await ctx.send(f"Pong! {round(self.bot.latency * 1000)}ms")
 
 def setup(bot):
     bot.add_cog(Ping(bot))
